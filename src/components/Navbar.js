4328512ebd4e4d859a25d013cdf2106c43ebd251
quanNguyen0872 <quan14139@gmail.com>
2022-11-26 19:15:32 +0700

    upadte navbar menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Tour & Travel</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href="/">
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign up</button>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
