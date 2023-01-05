12aaa716e0f7b717e932f5dcca383ad780148d5f
quanNguyen0872 <quan14139@gmail.com>
2023-01-05 23:37:14 +0700

    finish contact screen

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import ContactImg from "../assets/2.jpg";
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
 
 function Contact() {
   return (
@@ -14,6 +15,7 @@ function Contact() {
         title="Contact"
         btnClass="hide"
       />
+      <ContactForm />
       <Footer />
     </div>
   );
