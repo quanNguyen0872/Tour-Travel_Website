16f66957c5c30785fb129ef016dd40b7493d1c93
quanNguyen0872 <quan14139@gmail.com>
2023-01-05 19:28:14 +0700

    add trip component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Destination from "../components/Destination";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
 
 function Home() {
   return (
@@ -17,6 +18,7 @@ function Home() {
         btnClass="show"
       />
       <Destination />
+      <Trip />
     </div>
   );
 }
