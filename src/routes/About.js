734aedac32c96cb32d5e7299e9720c9cb7a25d06
quanNguyen0872 <quan14139@gmail.com>
2023-01-05 23:16:12 +0700

    finish about screen

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,12 +3,14 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import AboutImg from "../assets/night.jpg";
 import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 function About() {
   return (
     <div>
       <Navbar />
       <Hero cName="hero-mid" heroImg={AboutImg} title="About" btnClass="hide" />
+      <AboutUs />
       <Footer />
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Trip from "../components/Trip";
+import Home1 from "../assets/12.jpg";
 
 function Home() {
   return (
@@ -11,7 +12,7 @@ function Home() {
       <Navbar />
       <Hero
         cName="hero"
-        heroImg="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=974&q=80"
+        heroImg={Home1}
         title="Your Journey Your Story"
         text="Choose Your Favourite Destination"
         buttonText="Travel Plan"
